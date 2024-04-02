8ce02897c7ba31bf5ca8efada64c9eb161455c27
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2024-04-02 20:05:45 +0200

    menú creado

--- MAQUINA_VIRTUAL/src/main/Engine.java
+++ MAQUINA_VIRTUAL/src/main/Engine.java
@@ -1,5 +1,12 @@
 package main;
 
 public class Engine {
-
+	public void menu() {
+		System.out.println("HELP - muestra de nuevo este menú");
+		System.out.println("QUIT - cierra la aplicación");
+		System.out.println("RUN - ejecuta el programa");
+		System.out.println("NEWINST BYTECODE - introduce una nueva instrucción al programa");
+		System.out.println("RESET - vacía el programa actual");
+		System.out.println("REPLACE N - reemplaza la instrucción N por una nueva");
+	}
 }
--- MAQUINA_VIRTUAL/src/main/Main.java
+++ MAQUINA_VIRTUAL/src/main/Main.java
@@ -3,7 +3,8 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Engine eng = new Engine();
+		eng.menu();
 
 	}
 
