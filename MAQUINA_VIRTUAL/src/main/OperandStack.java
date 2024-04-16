86ef419cd9bb4b6c87502b83e629cc21b63d2f07
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2024-04-16 17:27:14 +0200

    clase OperandStack solo falta crear metodo toString()

--- MAQUINA_VIRTUAL/src/main/OperandStack.java
+++ MAQUINA_VIRTUAL/src/main/OperandStack.java
@@ -23,13 +23,13 @@ public class OperandStack {
 	}
 
 	/**
-	 * Metodo que convierte el array en un String. Retorna los elementos del array de manera formal pero no los imprime,
-	 * se utilizará en otros métodos.
+	 * Metodo que convierte el array en un String. Retorna los elementos del array
+	 * de manera formal pero no los imprime, se utilizará en otros metodos.
 	 * 
 	 * @return Retorna el array convertido en un String
 	 */
 	public String toString() {
-		//retorna la cadena de String si tiene algo
+		// retorna la cadena de String si tiene algo
 		return chain;
 
 	}
@@ -45,37 +45,57 @@ public class OperandStack {
 	}
 
 	/**
-	 * Metodo que anade elementos al array.
+	 * Metodo que aniade elementos al array.
 	 * 
-	 * Complejidad =
+	 * @param element es el elemento que se desea aniadir en el array stack
 	 * 
-	 * @param element es el elemento que se desea anadir en el array stack
-	 * 
-	 * @return Retorna true si se ha podido anadir el elemento correctamente, sino false
+	 * @return Retorna true si se ha podido aniadir el elemento correctamente, sino
+	 *         false
 	 */
 	public boolean push(int element) {
-		
+		if (this.stack.length < this.MAX_STACK) {
+			this.stack[this.num_elements] = element;
+			this.num_elements++;
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 
 	/**
-	 * Metodo que coge el ultimo elemento del array y lo elimina. El último elemento es num_element -1
+	 * Metodo que coge el ultimo elemento del array y lo elimina. El ultimo elemento
+	 * es num_element -1
 	 * 
-	 * Complejidad =
+	 * Complejidad = 0(1) ya que no variara en funcion del tamanio del dato
 	 * 
 	 * @return Retorna -1 si el array esta vacio, sino retorna el elemento que se
 	 *         encuentre en la ultima posicion
 	 */
 	public int pop() {
+		if(this.isEmpty()) {
+			return -1;
+		}else {
+			int eliminar;
+			eliminar = this.stack[this.num_elements - 1];
+			return eliminar;
+			
+		}
 
 	}
 
 	/**
 	 * Metodo que nos indica cual es el ultimo elemento del array
 	 * 
-	 * @return Retorna -1 si no es posible retornar la cima, sino devuelve el ultimo elemento
+	 * @return Retorna -1 si no es posible retornar la cima, sino devuelve el ultimo
+	 *         elemento
 	 */
 	public int getCima() {
-		System.out.println("El elemento que se encuentra en la cima de la pila es " + this.stack[this.num_elements - 1]);
+		if(this.isEmpty()) {
+			return -1;
+		}else {
+			return this.stack[this.num_elements - 1];
+		}
 	}
 
 }
