4ce4c61df1c14a4efbac3447a3337157e8d388a0
elenalf11 <elena.lopezfelix@colegiolitterator.com>
2024-04-16 19:10:52 +0200

    creacion de algunos metodos en Memory

--- MAQUINA_VIRTUAL/src/main/Main.java
+++ MAQUINA_VIRTUAL/src/main/Main.java
@@ -7,17 +7,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		OperandStack obj1 = new OperandStack();
-		obj1.push(5);
-		obj1.push(4);
-		obj1.push(10);
-		obj1.push(3);
-		obj1.push(7);
-		System.out.println(obj1.toString());
-		System.out.println("La cima de la pila es " + obj1.getCima());
-		obj1.pop();
-		System.out.println(obj1.toString());
-		System.out.println("La cima de la pila es " + obj1.getCima());
+		
 	
 
 	}
--- MAQUINA_VIRTUAL/src/main/Memory.java
+++ MAQUINA_VIRTUAL/src/main/Memory.java
@@ -26,12 +26,25 @@ public class Memory {
 	}
 
 	/**
-	 * Metodo que convierte el array en un String. Forma toda la linea de codigo que tiene que imprimir
+	 * Metodo que convierte el array en un String. Forma toda la linea de codigo que
+	 * tiene que imprimir
 	 * 
 	 * @return Retorna el array convertido en un String
 	 */
 	public String toString() {
-		//retorna la cadena formada
+		String chain = "Memoria:";
+		if (isEmpty) {
+			chain += " <vacía>";
+		} else {
+			int i = 0;
+			while (i < this.size) {
+				if (this.memory[i] != null) {
+					String numero = " [" + i + "] " + this.memory[i] + "  ";
+					chain += numero;
+				}
+				i++;
+			}
+		}
 		return chain;
 
 	}
@@ -44,12 +57,28 @@ public class Memory {
 	 * @return Retorna true si se ha podido escribir, sino retorna false
 	 */
 	public boolean write(int pos, int value) {
-		//if(pos >= 0)
-		//else
-		//return false
-		// se llama a resize y que compruebe si se necesita hacer un resize 
-		//this.memory[pos] = value; //se escribe el valor
-		
+		if (pos >= 0) {
+			if (pos <= this.size) {
+				this.memory[pos] = value;
+				this.size++;
+				this.isEmpty = false;
+				return true;
+			} else {
+				this.resize(pos);
+				this.memory[pos] = value;
+				this.isEmpty = false;
+				return true;
+			}
+
+		} else {
+			return false;
+		}
+		// if(pos >= 0)
+		// else
+		// return false
+		// se llama a resize y que compruebe si se necesita hacer un resize
+		// this.memory[pos] = value; //se escribe el valor
+
 	}
 
 	/**
@@ -59,23 +88,29 @@ public class Memory {
 	 * @return Retorna el valor que ha leido el metodo
 	 */
 	public Integer read(int pos) {
-		if(this.memory[pos] == null) {
+		if (this.memory[pos] == null) {
 			return -1;
-		}else {
+		} else {
 			return this.memory[pos];
 		}
-		
+
 	}
 
 	/**
 	 * Metodo que va aumentando el tamaño de nuestro array
 	 */
 	private void resize(int pos) {
-		//empty = false; la pone a false ya que significa que el array no va a estar vacio
-		//if(pos>= this.size){
-			//Aumentamos la posicion al doble al igual que el size
-		//hay que crear un nuevo array cuyo tamaño sea pos * 2
-		//se copian todos los valores que habian en el antiguo array, el resto de posiciones que no esten ocupadas se ponen a null
+		this.isEmpty = false;
+		if(pos >= this.size) {
+			
+		}
+		// empty = false; la pone a false ya que significa que el array no va a estar
+		// vacio
+		// if(pos>= this.size){
+		// Aumentamos la posicion al doble al igual que el size
+		// hay que crear un nuevo array cuyo tamaño sea pos * 2
+		// se copian todos los valores que habian en el antiguo array, el resto de
+		// posiciones que no esten ocupadas se ponen a null
 		// this.memory = new array (array 2)
 	}
 
